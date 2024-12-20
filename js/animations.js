7584903bbcb83faf6f183d2205f0cd1d172f801d
Christelle Couchoux <christelle.couchoux@gmail.com>
2024-12-20 14:48:35 +0100

    Projects - Only show 2 projects

--- js/animations.js
+++ js/animations.js
@@ -59,6 +59,12 @@ window.addEventListener('scroll', checkPositionTitles);
 
 const hiddenProjects = document.querySelectorAll('.hidden-project');
 
+const seeMoreProjects = document.getElementById('see-more-projects');
+const seeFewerProjects = document.getElementById('see-fewer-projects');
+const extraProjects = document.getElementById('more-projects');
+const firstExtraProject = document.getElementById('first-extra-project');
+
+
 function initProjects() {
     for (let i = 0; i < hiddenProjects.length; i++) {
         const hiddenProject = hiddenProjects[i];
@@ -71,12 +77,48 @@ function checkPositionProjects() {
         const hiddenProject = hiddenProjects[i];
         const positionFromTop = hiddenProjects[i].getBoundingClientRect().top;
 
-        if (positionFromTop <= 500) {
+        if (positionFromTop <= 500 && !extraProjects.contains(hiddenProject)) {
+            hiddenProject.classList.add('fade-in-project');
+            hiddenProject.classList.remove('hidden-js');
+        };
+
+        if (positionFromTop <= 500 
+            && extraProjects.contains(hiddenProject)
+            && seeMoreProjects.classList.contains("hidden-more-projects")
+        ) {
             hiddenProject.classList.add('fade-in-project');
             hiddenProject.classList.remove('hidden-js');
         };
     };
 };
 
+
+function toggleMoreProjects() {
+    seeMoreProjects.addEventListener('click', function() {
+        seeFewerProjects.classList.toggle('hidden-more-projects');
+        seeMoreProjects.classList.toggle('hidden-more-projects');
+        extraProjects.classList.toggle('hidden-more-projects');
+        firstExtraProject.classList.add('fade-in-project');
+        firstExtraProject.classList.remove('hidden-js');
+    })
+
+    seeFewerProjects.addEventListener('click', function() {
+        seeFewerProjects.classList.toggle('hidden-more-projects');
+        seeMoreProjects.classList.toggle('hidden-more-projects');
+        extraProjects.classList.toggle('hidden-more-projects');
+
+        for (let i = 0; i < hiddenProjects.length; i++) {
+            const hiddenProject = hiddenProjects[i];
+    
+            if (extraProjects.contains(hiddenProject)) {
+                hiddenProject.classList.remove('fade-in-project');
+                hiddenProject.classList.add('hidden-js');
+            }
+        };
+    })
+};
+
+
 initProjects();
 window.addEventListener('scroll', checkPositionProjects);
+toggleMoreProjects();
